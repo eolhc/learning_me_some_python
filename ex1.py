24f04294ee0d3f7324e8fc1fb744b7e93d503600
Chloe Phua <cqphua@gmail.com>
2017-01-18 17:26:19 +1100

    complete ex1-5

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# print 'hello world!'
+# print 'hello again!'
+# print 'i like typing this'
+# print 'This is fun.'
+# print 'yay! printing'
+# print "I'd much rather you 'not."
+#print 'I "said" do not touch this.'
+print 'this is a fancy new line'
---
+++ ex3.py
@@ -0,0 +1,22 @@
+#string that describes task
+print 'I will now only count my chickens:';
+#string followed by comma, followed by numbers for calculation
+print 'Hens', 25 + 30/6;
+print 'Roosters', 100 - 25 * 3 % 4
+#string that describes task
+print 'Now I will count the eggs:'
+#line equation whereby evaluated output is printed
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#prints a string
+print 'is it true that 3 + 2 < 5 - 7'
+#prints a boolean/ evaluates whether true or false
+print 3 + 2 < 5 - 7
+#and SO ON !
+print "What is 3 + 2?", 3 + 2
+print 'What is 5 - 7?', 5 - 7
+print "Oh, that's why it's false"
+print 'How about some more.'
+print "Is it greater?", 5 > - 2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+print 2/ float(3)
